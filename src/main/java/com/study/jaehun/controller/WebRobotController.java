780adcc4d77d5c06a9ebfe2d9f9c9693bd1c796e
Jaehun <jaehn95@gmail.com>
2021-09-27 17:51:27 +0900

    Add Hibernate Search, 검색기능 추가 ver1

--- src/main/java/com/study/jaehun/controller/WebRobotController.java
+++ src/main/java/com/study/jaehun/controller/WebRobotController.java
@@ -2,6 +2,7 @@ package com.study.jaehun.controller;
 
 import com.study.jaehun.dto.CreateDeveloper;
 import com.study.jaehun.dto.WebRobotUrl;
+import com.study.jaehun.service.SearchService;
 import com.study.jaehun.service.WebRobotService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -16,8 +17,9 @@ import java.io.IOException;
 @RequiredArgsConstructor
 public class WebRobotController {
     private final WebRobotService webRobotService;
+    private final SearchService searchService;
     @GetMapping("/test")
-    public String test() throws IOException {
-        return webRobotService.getPrice("https://tres-bien.com/dries-van-noten-heyze-pr-printed-top-black-212-021199-3221-900-fw21");
+    public void test() throws IOException {
+        searchService.searchWebpageTest("jacket");
     }
 }
